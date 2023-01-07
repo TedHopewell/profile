90d227f04c0c8dba77e023a66aaf799ab3e1185a
TedHopewell <hopewellmaome@gmail.com>
2023-01-08 00:14:26 +0200

    profile populated and updated with more info

--- src/Projects/ProjectData.js
+++ src/Projects/ProjectData.js
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import newsappImg from '../assets/image1.png'
 import weatherappImg from '../assets/image2.png'
 import planetImage from '../assets/image3.png'
@@ -7,14 +7,80 @@ import empImg from '../assets/image5.png'
 import myImage from '../assets/image6.png'
 import myImage2 from '../assets/image7.png'
 import myImage3 from '../assets/image8.png'
+import whatsapp from '../assets/Screenshot 2022-12-22 063415.png'
+import flagScammers from '../assets/s2.png'
+import restaurant from '../assets/rest.png'
 
 
 
 const ProjectData = [
 
+   {
+      title: "Flag Scammers",
+      description: "A React Native App for Jobseekers",
+      codeLink: "https://github.com/LindokuhleMnguni56/Flag-scammers",
+      projectLink: "",
+      imageUrl: flagScammers,
+      tag: "React Native",
+   },
+
+   {
+      title: "Food App",
+      description: "A React Native App for ordering food",
+      codeLink: "https://github.com/LindokuhleMnguni56/Restaurant-App",
+      projectLink: "",
+      imageUrl: restaurant,
+      tag: "React Native",
+   },
+
+   {
+      title: "Whatsapp",
+      description: "A Whatsapp Clone with MERN stack",
+      codeLink: "https://github.com/TedHopewell/Whatsapp-Clone-MERN-",
+      projectLink: "",
+      imageUrl: whatsapp,
+      tag: "ReactJS",
+     },
+     
+     {
+      title: "Audio-Recorder",
+      description: "A React Native Audio Recorder App",
+      codeLink: "https://github.com/TedHopewell/Voice-Recorder",
+      projectLink: "",
+      imageUrl: myImage3,
+      tag: "React Native",
+     },
+
+     {
+      title: "Budget App",
+      description: "A fullstack budget app",
+      codeLink: "https://github.com/TedHopewell/budget-app",
+      projectLink: "https://budget-app-14541.web.app/",
+      imageUrl: weatherappImg,
+      tag: "ReactJS",
+   },
+
+   {
+      title: "Employee CRUD App",
+      description: "A fullstack Employee app",
+      codeLink: "https://github.com/TedHopewell/Employee-app",
+      projectLink: "https://employee-app-d49b6.web.app",
+      imageUrl: newsappImg,
+      tag: "ReactJS",
+   },
+
+   {
+      title: "To-do list",
+      description: "A fullstack Task Manager",
+      codeLink: "https://github.com/TedHopewell/Task-Manager-App",
+      projectLink: "https://task-manager-app-659ca.web.app",
+      imageUrl: planetImage,
+      tag: "ReactJS",
+   },
+
       {
          title: "Base-Apparel",
-         description: "A coming soon page running on Desktop",
+         description: "A Frontend coming soon page",
          codeLink: "https://github.com/TedHopewell/Base-Apparel",
          projectLink: "",
          imageUrl: myImage,
@@ -23,35 +89,16 @@ const ProjectData = [
 
       {
          title: "Bookstore",
-         description: "A bookstore website running on both mobile and web",
+         description: "A Fullstack bookstore website",
          codeLink: "https://github.com/TedHopewell/bookstore-cart",
          projectLink: "",
          imageUrl: myImage2,
          tag: "Javascript CSS and HTML",
       },
 
-      
-      {
-        title: "Budget App",
-        description: "A fullstack budget app running on mobile",
-        codeLink: "https://github.com/TedHopewell/budget-app",
-        projectLink: "https://budget-app-14541.web.app/",
-        imageUrl: weatherappImg,
-        tag: "ReactJS",
-     },
-
-     {
-        title: "Employee App",
-        description: "A fullstack Employee app running on mobile",
-        codeLink: "https://github.com/TedHopewell/Employee-app",
-        projectLink: "https://employee-app-d49b6.web.app",
-        imageUrl: newsappImg,
-        tag: "ReactJS",
-     },
-
      {
         title: "Counter App",
-        description: "A fullstack Counter app running on mobile",
+        description: "A fullstack Redux Counter app",
         codeLink: "https://github.com/TedHopewell/counter-app",
         projectLink: "https://counter-app-840fd.web.app/",
         imageUrl: onBoardImg,
@@ -60,31 +107,17 @@ const ProjectData = [
 
      {
         title: "Covid App",
-        description: "A fullstack Covid app running on mobile",
+        description: "A fullstack Covid app",
         codeLink: "https://github.com/TedHopewell/covid-19-Tracker",
         projectLink: "https://covid-tracker-5a406.web.app",
         imageUrl: empImg,
         tag: "ReactJS",
      },
 
-     {
-        title: "To-do list",
-        description: "A fullstack Task Manager app running on mobile",
-        codeLink: "https://github.com/TedHopewell/Task-Manager-App",
-        projectLink: "https://task-manager-app-659ca.web.app",
-        imageUrl: planetImage,
-        tag: "ReactJS",
-     },
+    
 
-     {
-      title: "Audio-Recorder",
-      description: "A React Native Audio Recorder App",
-      codeLink: "https://github.com/TedHopewell/Voice-Recorder",
-      projectLink: "",
-      imageUrl: myImage3,
-      tag: "ReactJS",
-     },
-   
+
+     
      
 ]
    
